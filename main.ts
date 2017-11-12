d1a926791f055c94f03208008b44e080136bb155
Paul Boon <gvr37.p.l.boon@gmail.com>
2017-11-12 21:11:51 +0100

    p

--- main.ts
+++ main.ts
@@ -6,38 +6,86 @@
 var canctx = createCanvas(600,450)
 var canvas = canctx.canvas
 var ctxt = canctx.ctxt
-canvas.style.backgroundColor = 'lightgrey'
+canvas.style.backgroundColor = 'grey'
+
+var canvaselement = document.querySelector('#canvas')
+var buttonselement = document.querySelector('#buttons')
+var buttonsReelement = document.querySelector('#buttonsRe')
+var specialselement = document.querySelector('#specials')
+canvaselement.appendChild(canvas)
 
 var cube = new Cube()
 cube.draw(ctxt)
 
-new Button(document.body,'F','',() => {
+new Button(specialselement,'scramble','',() => {
+    cube.scramble(50)
+    cube.draw(ctxt)
+})
+
+new Button(specialselement,'solve','',() => {
+    cube.solve()
+    cube.draw(ctxt)
+})
+
+new Button(buttonselement,'F','',() => {
     cube.F(false)
     cube.draw(ctxt)
 })
 
-new Button(document.body,'B','',() => {
+new Button(buttonselement,'B','',() => {
     cube.B(false)
     cube.draw(ctxt)
 })
 
-new Button(document.body,'L','',() => {
+new Button(buttonselement,'L','',() => {
     cube.L(false)
     cube.draw(ctxt)
 })
 
-new Button(document.body,'R','',() => {
+new Button(buttonselement,'R','',() => {
     cube.R(false)
     cube.draw(ctxt)
 })
 
-new Button(document.body,'U','',() => {
+new Button(buttonselement,'U','',() => {
     cube.U(false)
     cube.draw(ctxt)
 })
 
-new Button(document.body,'D','',() => {
+new Button(buttonselement,'D','',() => {
     cube.D(false)
+    cube.draw(ctxt)
+})
+
+
+
+new Button(buttonsReelement,'F`','',() => {
+    cube.F(true)
+    cube.draw(ctxt)
+})
+
+new Button(buttonsReelement,'B`','',() => {
+    cube.B(true)
+    cube.draw(ctxt)
+})
+
+new Button(buttonsReelement,'L`','',() => {
+    cube.L(true)
+    cube.draw(ctxt)
+})
+
+new Button(buttonsReelement,'R`','',() => {
+    cube.R(true)
+    cube.draw(ctxt)
+})
+
+new Button(buttonsReelement,'U`','',() => {
+    cube.U(true)
+    cube.draw(ctxt)
+})
+
+new Button(buttonsReelement,'D`','',() => {
+    cube.D(true)
     cube.draw(ctxt)
 })
 
